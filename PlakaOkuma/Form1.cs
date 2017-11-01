e61c08fa3222695c72d571520d6e305c9dd0b97d
vnoisy <berkcanpekatik@gmail.com>
2017-11-01 13:17:39 +0200

    New Changes

---
+++ PlakaOkuma/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using openalprnet;
+
+//bu proje openALPR kütüphanesi kullanarak yapılmıştır
+//https://github.com/openalpr
+//Berk Pekatik 20161132079
+
+namespace PlakaTanima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public Rectangle boundingRectangle(List<Point> points)
+        {
+            // Add checks here, if necessary, to make sure that points is not null,
+            // and that it contains at least one (or perhaps two?) elements
+
+            var minX = points.Min(p => p.X);
+            var minY = points.Min(p => p.Y);
+            var maxX = points.Max(p => p.X);
+            var maxY = points.Max(p => p.Y);
+
+            return new Rectangle(new Point(minX, minY), new Size(maxX - minX, maxY - minY));
+        }
+
+        private static Image cropImage(Image img, Rectangle cropArea)
+        {
+            var bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+
+        public static Bitmap combineImages(List<Image> images)
+        {
+            //read all images into memory
+            Bitmap finalImage = null;
+
+            try
+            {
+                var width = 0;
+                var height = 0;
+
+                foreach (var bmp in images)
+                {
+                    width += bmp.Width;
+                    height = bmp.Height > height ? bmp.Height : height;
+                }
+
+                //create a bitmap to hold the combined image
+                finalImage = new Bitmap(width, height);
+
+                //get a graphics object from the image so we can draw on it
+                using (var g = Graphics.FromImage(finalImage))
+                {
+                    //set background color
+                    g.Clear(Color.Black);
+
+                    //go through each image and draw it on the final image
+                    var offset = 0;
+                    foreach (Bitmap image in images)
+                    {
+                        g.DrawImage(image,
+                                    new Rectangle(offset, 0, image.Width, image.Height));
+                        offset += image.Width;
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally
+            {
+                //clean up memory
+                foreach (var image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+
+        private void processImageFile(string fileName)
+        {
+            sifirla();
+            var region = "eu";
+            String config_file = Path.Combine(AssemblyDirectory, "openalpr.conf");
+            String runtime_data_dir = Path.Combine(AssemblyDirectory, "runtime_data");
+            using (var alpr = new AlprNet(region, config_file, runtime_data_dir))
+            {
+                if (!alpr.IsLoaded())
+                {
+                    plakaYaz.Text = ("Error initializing OpenALPR");
+                    return;
+                }
+                aracFoto.ImageLocation = fileName;
+                aracFoto.Load();
+
+                var results = alpr.Recognize(fileName);
+
+                var images = new List<Image>(results.Plates.Count());
+                var i = 1;
+                foreach (var result in results.Plates)
+                {
+                    var rect = boundingRectangle(result.PlatePoints);
+                    var img = Image.FromFile(fileName);
+                    var cropped = cropImage(img, rect);
+                    images.Add(cropped);
+                    string[] plakaList = result.TopNPlates.Select(x => x.Characters).ToArray();
+                    plakaYaz.Text = plakaList[0].ToString();
+                }
+
+                if (images.Any())
+                {
+                    aracPlaka.Image = combineImages(images);
+                }
+            }
+        }
+
+        private void sifirla()
+        {
+            aracFoto.Image = null;
+            aracPlaka.Image = null;
+            plakaYaz.Text = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sifirla();
+        }
+
+        private void plakaBul_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                processImageFile(openFileDialog.FileName);
+            }
+        }
+
+    }
+}
